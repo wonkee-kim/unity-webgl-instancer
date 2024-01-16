e5f77b44ddfe31783fa7e25d971ddd6ad7443460
Michael Jurka <mikejurka@gmail.com>
2024-01-16 01:16:56 -0500

    more debug code

--- unity-webgl-instancer-unity/Assets/Instancer/Scripts/Demo/ZombieGenerator.cs
+++ unity-webgl-instancer-unity/Assets/Instancer/Scripts/Demo/ZombieGenerator.cs
@@ -3,102 +3,154 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SpatialSys.UnitySDK;
+using UnityEngine.SceneManagement;
 
 public class ZombieGenerator : MonoBehaviour
 {
     //public static ZombieGenerator instance { get; private set; }
 
-    [SerializeField] private GameObject _saveManager;
-    private int _killCount = 0;
+    // [SerializeField] private GameObject _saveManager;
+    // private int _killCount = 0;
 
     private const int MAX_ZOMBIE_COUNT = 1000;
 
-    [SerializeField] private GameObject _zombiePrefab;
+    // [SerializeField] private GameObject _zombiePrefab;
     private List<ZombieBehaviour> _zombiesPool = new List<ZombieBehaviour>(MAX_ZOMBIE_COUNT);
-/*
-    private const float SPAWN_TIME = 0.03f;
-    private const float SPAWN_RADIUS = 30f;
+    //private ArrayList _zombiesPool = new ArrayList(MAX_ZOMBIE_COUNT);
 
-    private float _lastSpawnTime = 0f;
-
-    //private Delegate _customEvHandler;
-
-    private void Awake()
+    void PrintAllScenes()
     {
-        Debug.Log("AWAKE?!");
-        //instance = this;
-        //_customEvHandler = VisualScriptingUtility.AddCustomEventListener(gameObject, HandleCustomEvent);
-    }
-    private void OnDestroy()
-    {
-        //VisualScriptingUtility.RemoveCustomEventListener(gameObject, _customEvHandler);
-    }
-    private void HandleCustomEvent(string message, object[] args)
-    {
-        switch (message)
+        for (int i = 0; i < SceneManager.sceneCount; i++)
         {
-            case "Initialized":
-                //UpdateKillCount(addCount: (int)args[0]); // current score + loaded scores
-                break;
-            default:
-                Debug.LogWarning("received unknown message: " + message);
-                break;
+            Scene scene = SceneManager.GetSceneAt(i);
+            Debug.Log("Scene: " + scene.name);
+            PrintSceneObjects(scene.GetRootGameObjects(), 1);
         }
+    }
+
+    void PrintSceneObjects(GameObject[] objects, int depth)
+    {
+        string indentation = new string('_', depth * 4);
+
+        foreach (GameObject obj in objects)
+        {
+            Debug.Log(indentation + "* " + obj.name);
+            PrintSceneObjects(GetChildObjects(obj), depth + 1);
+        }
+    }
+
+    GameObject[] GetChildObjects(GameObject obj)
+    {
+        GameObject[] children = new GameObject[obj.transform.childCount];
+
+        for (int i = 0; i < obj.transform.childCount; i++)
+        {
+            children[i] = obj.transform.GetChild(i).gameObject;
+        }
+
+        return children;
+    }
+
+    void PrintDontDestroyOnLoadObjects()
+    {
+        GameObject temp = new GameObject();
+        DontDestroyOnLoad(temp);
+        Scene dontDestroyOnLoadScene = temp.scene;
+        Destroy(temp);
+
+        Debug.Log("DontDestroyOnLoad Objects:");
+        PrintSceneObjects(dontDestroyOnLoadScene.GetRootGameObjects(), 1);
     }
 
     private void Start()
     {
-        for (int i = 0; i < MAX_ZOMBIE_COUNT; i++)
-        {
-            GameObject zombie = Instantiate(_zombiePrefab, transform);
-            zombie.SetActive(false);
-            _zombiesPool.Add(zombie.GetComponent<ZombieBehaviour>());
-        }
-        SpawnZombie(100);
+        PrintAllScenes();
+        PrintDontDestroyOnLoadObjects();
     }
+    /*
+        private const float SPAWN_TIME = 0.03f;
+        private const float SPAWN_RADIUS = 30f;
 
-    private void Update()
-    {
-        if (Time.time - _lastSpawnTime > SPAWN_TIME)
+        private float _lastSpawnTime = 0f;
+
+        //private Delegate _customEvHandler;
+
+        private void Awake()
         {
-            _lastSpawnTime = Time.time;
-            SpawnZombie(20);
+            Debug.Log("AWAKE?!");
+            //instance = this;
+            //_customEvHandler = VisualScriptingUtility.AddCustomEventListener(gameObject, HandleCustomEvent);
         }
-    }
-
-    private void SpawnZombie(int count)
-    {
-        for (int i = 0; i < count; i++)
+        private void OnDestroy()
         {
-            float theta = UnityEngine.Random.Range(0f, Mathf.PI * 2f);
-            Vector3 spawnPosition = new Vector3(Mathf.Cos(theta), 0f, Mathf.Sin(theta)) * SPAWN_RADIUS;
-            spawnPosition += Player.position;
-
-            ZombieBehaviour zombie = _zombiesPool.Find(z => !z.gameObject.activeSelf);
-            // if (zombie == null)
-            // {
-            //     GameObject zombieObject = Instantiate(_zombiePrefab, spawnPosition, Quaternion.identity, transform);
-            //     zombie = zombieObject.GetComponent<ZombieBehaviour>();
-            //     _zombiesPool.Add(zombie);
-            // }
-
-            if (zombie != null)
+            //VisualScriptingUtility.RemoveCustomEventListener(gameObject, _customEvHandler);
+        }
+        private void HandleCustomEvent(string message, object[] args)
+        {
+            switch (message)
             {
-                zombie.Spawn(spawnPosition);
-                zombie.gameObject.SetActive(true);
+                case "Initialized":
+                    //UpdateKillCount(addCount: (int)args[0]); // current score + loaded scores
+                    break;
+                default:
+                    Debug.LogWarning("received unknown message: " + message);
+                    break;
             }
         }
-    }
 
-    // public static void AddKill()
-    // {
-    //     instance.UpdateKillCount(1);
-    // }
-    // private void UpdateKillCount(int addCount)
-    // {
-    //     _killCount += addCount;
-    //     UIManager.UpdateKillCount(_killCount);
-    //     VisualScriptingUtility.TriggerCustomEvent(_saveManager, "SaveScore", new object[] { _killCount });
-    // }
-    */
+        private void Start()
+        {
+            for (int i = 0; i < MAX_ZOMBIE_COUNT; i++)
+            {
+                GameObject zombie = Instantiate(_zombiePrefab, transform);
+                zombie.SetActive(false);
+                _zombiesPool.Add(zombie.GetComponent<ZombieBehaviour>());
+            }
+            SpawnZombie(100);
+        }
+
+        private void Update()
+        {
+            if (Time.time - _lastSpawnTime > SPAWN_TIME)
+            {
+                _lastSpawnTime = Time.time;
+                SpawnZombie(20);
+            }
+        }
+
+        private void SpawnZombie(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                float theta = UnityEngine.Random.Range(0f, Mathf.PI * 2f);
+                Vector3 spawnPosition = new Vector3(Mathf.Cos(theta), 0f, Mathf.Sin(theta)) * SPAWN_RADIUS;
+                spawnPosition += Player.position;
+
+                ZombieBehaviour zombie = _zombiesPool.Find(z => !z.gameObject.activeSelf);
+                // if (zombie == null)
+                // {
+                //     GameObject zombieObject = Instantiate(_zombiePrefab, spawnPosition, Quaternion.identity, transform);
+                //     zombie = zombieObject.GetComponent<ZombieBehaviour>();
+                //     _zombiesPool.Add(zombie);
+                // }
+
+                if (zombie != null)
+                {
+                    zombie.Spawn(spawnPosition);
+                    zombie.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        // public static void AddKill()
+        // {
+        //     instance.UpdateKillCount(1);
+        // }
+        // private void UpdateKillCount(int addCount)
+        // {
+        //     _killCount += addCount;
+        //     UIManager.UpdateKillCount(_killCount);
+        //     VisualScriptingUtility.TriggerCustomEvent(_saveManager, "SaveScore", new object[] { _killCount });
+        // }
+        */
 }
